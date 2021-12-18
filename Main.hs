c14ea5a6b7e6dbcbd95081dadc4c524c6e060424
Simcha van Collem <simcha@vancollem.nl>
2021-12-18 17:37:21 +0100

    Added numbers to assumptions.
    These will be used to indicate which assumption are given-up where.
    The Solver monad is used to keep track of the number for the next assumtpion.

--- Main.hs
+++ Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
-import Control.Applicative ((<|>))
+import Control.Applicative (empty, (<|>))
 import Data.List (find)
+import Solver
+  ( Solver (solve),
+    getState,
+    liftMaybe,
+    modifyState,
+  )
 
 data Formula = Falsum | Basic Int | And Formula Formula | Or Formula Formula | Impl Formula Formula
   deriving (Show, Eq)
@@ -21,33 +27,47 @@ infixr 2 \/
 (\/) :: Formula -> Formula -> Formula
 (\/) = Or
 
-newtype Assumption = Assumption Formula
-  deriving (Show)
+type AssumptionCounter = Int
 
-data DeductionTree = Tree Formula [DeductionTree] | Assumption' Assumption
+data Assumption = Assumption Formula AssumptionCounter
   deriving (Show)
 
 type Theory = [Formula]
 
-proof :: Formula -> Theory -> [Assumption] -> Maybe DeductionTree
-proof f theory assumptions =
+data DeductionTree = Tree Formula (Maybe AssumptionCounter) [DeductionTree] | Assumption' Assumption
+  deriving (Show)
+
+proof :: Formula -> Theory -> Maybe DeductionTree
+proof f theory = fst <$> solve (proof' f theory []) 0
+
+proof' :: Formula -> Theory -> [Assumption] -> Solver AssumptionCounter DeductionTree
+proof' f theory assumptions =
   proofFromTheory f theory
     <|> proofFromAssumption f assumptions
     <|> proofByIntroduction f theory assumptions
 
-proofFromTheory :: Formula -> Theory -> Maybe DeductionTree
-proofFromTheory f theory = (\f -> Tree f []) <$> find (== f) theory
+proofFromTheory :: Formula -> Theory -> Solver AssumptionCounter DeductionTree
+proofFromTheory f theory = do
+  if f `elem` theory
+    then return $ Tree f Nothing []
+    else empty
 
-proofFromAssumption :: Formula -> [Assumption] -> Maybe DeductionTree
-proofFromAssumption f assumptions = Tree f . return . Assumption' <$> find (\(Assumption f') -> f == f') assumptions
+proofFromAssumption :: Formula -> [Assumption] -> Solver AssumptionCounter DeductionTree
+proofFromAssumption f assumptions = do
+  assumption <- liftMaybe $ find (\(Assumption f' _) -> f == f') assumptions
+  return $ Tree f Nothing [Assumption' assumption]
 
-proofByIntroduction :: Formula -> Theory -> [Assumption] -> Maybe DeductionTree
+proofByIntroduction :: Formula -> Theory -> [Assumption] -> Solver AssumptionCounter DeductionTree
 proofByIntroduction f theory assumptions = case f of
-  Falsum -> Nothing
-  Basic _ -> Nothing
-  And f1 f2 -> Tree f <$> sequence [proof f1 theory assumptions, proof f2 theory assumptions]
-  Or f1 f2 -> Tree f . return <$> proof f1 theory assumptions <|> proof f2 theory assumptions
-  Impl f1 f2 -> Tree f . return <$> proof f2 theory (Assumption f1 : assumptions)
+  Falsum -> empty
+  Basic _ -> empty
+  And f1 f2 -> Tree f Nothing <$> sequence [proof' f1 theory assumptions, proof' f2 theory assumptions]
+  Or f1 f2 -> Tree f Nothing . return <$> proof' f1 theory assumptions <|> proof' f2 theory assumptions
+  Impl f1 f2 -> do
+    assumptionCounter <- getState
+    modifyState (+ 1)
+    p <- proof' f2 theory (Assumption f1 assumptionCounter : assumptions)
+    return $ Tree f (Just assumptionCounter) [p]
 
 main :: IO ()
 main = undefined
