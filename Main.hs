3f4a844b9c251986f9335fb1e34ef44e947aec5f
Simcha van Collem <simcha@vancollem.nl>
2021-12-18 14:11:11 +0100

    Add proof from theory
    Formulas can now be proven if they're present in the theory

--- Main.hs
+++ Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List (find)
+
 data Formula = Falsum | Basic Int | And Formula Formula | Or Formula Formula | Impl Formula Formula
   deriving (Show, Eq)
 
@@ -27,7 +29,10 @@ data DeductionTree = Tree Formula [DeductionTree] | Assumption' Assumption
 type Theory = [Formula]
 
 proof :: Formula -> Theory -> [Assumption] -> Maybe DeductionTree
-proof = undefined
+proof f theory assumptions = proofFromTheory f theory
+
+proofFromTheory :: Formula -> Theory -> Maybe DeductionTree
+proofFromTheory f theory = (\f -> Tree f []) <$> find (== f) theory
 
 main :: IO ()
 main = undefined
